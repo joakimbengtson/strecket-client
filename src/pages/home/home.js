0cf30b8bbaacbec2f9aaac23f327a81fc9b96410
Joakim Bengtson <joakim@bookio.com>
2016-11-04 11:33:39 +0100

    -

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -27,7 +27,7 @@ module.exports = class Home extends React.Component {
 							<Jumbotron>
 						      <h1>Hello, world!</h1>
 						      <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-						      <p><Button bsStyle="primary">Learn more</Button></p>
+						      <p><Button bsStyle="primary">HEJ MEG</Button></p>
 						    </Jumbotron>
 						</Col>
 					</Row>
