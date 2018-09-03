8d9ae645c97e04699e0bd02322d56efc87dc53cb
meg768 <meg768@users.noreply.github.com>
2018-09-03 20:53:17 +0200

    -

--- src/pages/meg/meg.js
+++ src/pages/meg/meg.js
@@ -6,6 +6,115 @@ import sprintf from 'yow/sprintf';
 import QueryString from 'querystring';
 
 
+class StockChart extends React.Component {
+
+    constructor(args) {
+        super(args);
+
+        this.state = {};
+        this.state.ready = false;
+        this.state.config = {};
+        this.state.symbol = 'AAPL';
+
+    }
+
+    /*
+    componentDidUpdate(prevProps) {
+        this.generate();
+    }
+    */
+
+    componentDidMount() {
+        this.generate();
+    }
+
+
+    generate() {
+        var request = new Request('http://app-o.se:3012');
+        var query = {};
+        var params = {};
+        var now = new Date();
+        var then = new Date();
+
+        then.setFullYear(now.getFullYear() - 1);
+
+        query.sql        = 'select date, close from quotes where symbol = ? and date >= ?';
+        query.values     = [this.state.symbol, sprintf('%04d-%02d-%02d', then.getFullYear(), then.getMonth() + 1, then.getDate())];
+
+        var data = [];
+
+        request.get('/query', {query:query}).then(response => {
+            var stocks = response.body;
+
+            stocks.forEach(stock => {
+                var date = new Date(stock.date);
+                data.push([Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()), stock.close]);
+            });
+
+            var config = {
+              rangeSelector: {
+                selected: 1
+              },
+              title: {
+                text: this.state.symbol
+              },
+              subtitle: {
+                  text: 'Ett år tillbaka'
+              },
+
+              yAxis: {
+                  title: {
+                      text: 'Värde'
+                  }
+              },
+              xAxis: {
+                  type: 'datetime',
+                  dateTimeLabelFormats: {
+                      month: '%b',
+                      year: '%b'
+                  },
+                  title: {
+                      text: 'Datum'
+                  }
+              },
+              series: [{
+                name: this.state.symbol,
+                data: data,
+                tooltip: {
+                  valueDecimals: 2
+                }
+              }]
+            };
+
+            this.setState({ready:true, config:config});
+        })
+        .catch(error => {
+            console.log(error);
+        })
+
+    }
+
+    render() {
+        if (this.state.ready)
+            return (<ReactHighcharts config={this.state.config} ref="chart"></ReactHighcharts>)
+        else {
+            return (<div></div>);
+        }
+    }
+
+
+
+}
+
+module.exports = class Module extends React.Component {
+
+    render() {
+        return (
+            <StockChart symbol='TSLA'/>
+        );
+    }
+}
+/*
 module.exports = class Module extends React.Component {
 
     constructor(args) {
@@ -121,3 +230,4 @@ console.log(params);
         }
     }
 }
+*/
