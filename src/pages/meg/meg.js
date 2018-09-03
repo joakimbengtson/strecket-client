bdfecb845e44ca8b9d1a14e6ee8c73b5310c1275
meg768 <meg768@users.noreply.github.com>
2018-09-03 20:34:14 +0200

    -

---
+++ src/pages/meg/meg.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+module.exports = class Module extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+
+	};
+
+	// Virtuell function som anropas då sidan visas
+	componentDidMount() {
+
+
+	}
+
+
+	render() {
+
+		return (
+			<div>HEJ MEG</div>
+		);
+
+	};
+};
