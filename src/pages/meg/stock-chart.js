8cfa8c17ea18b1a333d17285a0813e45a8120f49
meg768 <meg768@users.noreply.github.com>
2018-09-03 21:46:42 +0200

    -

--- src/pages/meg/stock-chart.js
+++ src/pages/meg/stock-chart.js
@@ -11,41 +11,54 @@ module.exports = class StockChart extends React.Component {
         super(args);
 
         this.state = {};
+        // ready = false, dvs vi har inte läst in data än...
         this.state.ready = false;
-        this.state.config = {};
-        this.state.symbol = this.props.symbol;
 
+        // config = tomt än så länge.
+        this.state.config = {};
+
+        // Hämta parametrar från anropet <StockChart symbol='X'/>
+        this.state.symbol = this.props.symbol;
     }
 
+    // Anropas efter konponenten är skapad och finns i DOM:en
     componentDidMount() {
         this.generate();
     }
 
     generate() {
+        // Deklarera en request som går direkt till Munch (slipper då MySQL-anrop)
         var request = new Request('http://app-o.se:3012');
-        var query = {};
-        var params = {};
+
+        // Nu och då
         var now = new Date();
         var then = new Date();
 
+        // Då är ett år tillbaka i tiden
         then.setFullYear(now.getFullYear() - 1);
 
+        // Skapa läsbara texter från nu och då
         var nowYMD = sprintf('%04d-%02d-%02d', now.getFullYear(), now.getMonth() + 1, now.getDate());
         var thenYMD = sprintf('%04d-%02d-%02d', then.getFullYear(), then.getMonth() + 1, then.getDate());
 
+        // Skapa frågan
+        var query = {};
         query.sql        = 'select date, close from quotes where symbol = ? and date >= ?';
         query.values     = [this.state.symbol, thenYMD];
 
         var data = [];
 
+        // Hämta data frpn Munch via ett '/query' anrop...
         request.get('/query', {query:query}).then(response => {
             var stocks = response.body;
 
+            // Lägg till i vektorn 'data' på det format som Highcharts vill ha det
             stocks.forEach(stock => {
                 var date = new Date(stock.date);
                 data.push([Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()), stock.close]);
             });
 
+            // Skapa en Highcharts 'config'...
             var config = {
               rangeSelector: {
                 selected: 1
@@ -81,6 +94,7 @@ module.exports = class StockChart extends React.Component {
               }]
             };
 
+            // Sätt denna komponents 'tillstånd' till klar och datan finns under 'config'...
             this.setState({ready:true, config:config});
         })
         .catch(error => {
@@ -90,7 +104,10 @@ module.exports = class StockChart extends React.Component {
     }
 
     render() {
+        // Om allt är klart så...
         if (this.state.ready) {
+
+            // Lite styling kring grafen
             var style = {};
             style.border = '1px solid rgba(0, 0, 0, 0.1)';
             style.marginLeft = '10em';
@@ -98,14 +115,15 @@ module.exports = class StockChart extends React.Component {
             style.marginTop = '5em';
             style.marginBottom = '5em';
 
+            // Returnera grafen med angiven stil och genererad data...
             return (
-
                 <div style = {style}>
                     <ReactHighstock config={this.state.config} ref="chart"></ReactHighstock>
                 </div>
             );
 
         }
+        // Annars visa en tom graf...
         else {
             return (<div></div>);
         }
