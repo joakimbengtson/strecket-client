747fe6d219e160e31b5072309b68bc25c63a6c44
Joakim Bengtson <joakim@bookio.com>
2020-02-26 17:51:47 +0100

    -

--- src/pages/meg/meg.js
+++ src/pages/meg/meg.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import StockChartList from './stock-chart-list.js';
 import Request from 'yow/request';
+import {Container, Table, Row, Col} from 'react-bootify';
+
+const puppeteer = require('puppeteer');
 
 require('./meg.css'); 
 
@@ -28,12 +31,31 @@ class NagotSomFunkarBattreOmNagotBlirFel extends React.Component {
 	constructor(props) {
 		super(props);
 
-		this.state = {spikes:null, dates:null, error:null, sectors:[], tickers: ""};
+		this.state = {spikes:null, dates:null, error:null, sectors:[], tickers: "", mmth:"-"};
 		
 		this.handleCheck = this.handleCheck.bind(this);
 		this.handleChange = this.handleChange.bind(this);		
 		
 	};
+
+	getMMTH() {
+        return new Promise((resolve, reject) => {
+	        var request = new Request('http://app-o.se:3012');
+	        var query = {};
+	        
+	        query.sql = 'SELECT truncate(count(*)/(select count(*) from stocks)*100, 0) as mmth FROM stockquotes INNER JOIN stocks ON stocks.symbol = stockquotes.symbol where stockquotes.date=? and stockquotes.close > stocks.sma200';
+
+	        query.values = [this.state.dates[0]];
+
+	        request.get('/mysql', {query:query}).then(response => {
+                resolve(response.body[0].mmth);
+	        })
+	        .catch(error => {
+                reject(error);
+	        })
+        });
+		
+	}	
 	
 	getSectors() {
         return new Promise((resolve, reject) => {
@@ -43,7 +65,7 @@ class NagotSomFunkarBattreOmNagotBlirFel extends React.Component {
 	        
 	        query.sql = 'SELECT stocks.industry, sum(b.close>a.close)/count(b.close) as perc FROM stockquotes a INNER JOIN stockquotes b ON a.symbol = b.symbol INNER JOIN stocks ON stocks.symbol = a.symbol where a.date=? and b.date=? and sector <> "" and industry <> "" group by sector, industry order by perc desc';
 
-	        query.values = [this.state.dates[0], this.state.dates[1]];
+	        query.values = [this.state.dates[1], this.state.dates[0]];
 
 	        request.get('/mysql', {query:query}).then(response => {
 	            sectors = response.body;
@@ -79,17 +101,23 @@ class NagotSomFunkarBattreOmNagotBlirFel extends React.Component {
 	}
 
 	getFearAndGreed() {
+		axios.defaults.headers.post['Content-Type'] ='application/x-www-form-urlencoded';
+		
         return new Promise((resolve, reject) => {
-	      	var request = new Request('http://stackabuse.com');
-
-	        request.get().then(response => {
-			  console.log(response);
-			  resolve(response);
-	        })
-	        .catch(error => {
-                reject(error);
-			})
-        });		
+			axios.get('https://money.cnn.com/data/fear-and-greed/')
+			  .then(function (response) {
+			    console.log(response.data);
+			    console.log(response.status);
+			    console.log(response.statusText);
+			    console.log(response.headers);
+			    console.log(response.config);
+			    resolve(response.data);
+			  })
+			.catch((error) => {
+				console.log(error);
+				reject(error);				
+			})			  
+		});		
 	}
 
 	getSpikes() {
@@ -98,10 +126,10 @@ class NagotSomFunkarBattreOmNagotBlirFel extends React.Component {
 	        var query = {};
 	        var spikes = [];
 	        
-			_descr1 = "50% över normal volym, stängt över gårdagen, över 51 week high, över sma200, omsatt mer än 5 miljoner $";
-	        query.sql    = 'SELECT a.symbol, a.volume, b.volume, a.close as lastClose, b.close as previousClose FROM stockquotes a INNER JOIN stockquotes b ON a.symbol = b.symbol INNER JOIN stocks ON stocks.symbol = a.symbol WHERE a.date = ? AND b.date = ? AND a.volume > b.AV14*1.5 AND a.close > b.close AND a.close > a.SMA200 AND a.close*a.AV14 > 5000000 AND a.close > a.open AND a.close >= stocks.wh51';
+			_descr1 = "60% över normal volym, stängt över gårdagen, över 51 week high, över sma200, omsatt mer än 5 miljoner $";
+	        query.sql    = 'SELECT a.symbol, a.volume, b.volume, a.close as lastClose, b.close as previousClose FROM stockquotes a INNER JOIN stockquotes b ON a.symbol = b.symbol INNER JOIN stocks ON stocks.symbol = a.symbol WHERE a.date = ? AND b.date = ? AND a.volume > b.AV14*1.6 AND a.close > b.close AND a.close > a.SMA200 AND a.close*a.AV14 > 5000000 AND a.close > a.open AND a.close >= stocks.wh51';
 
-	        query.values = [this.state.dates[0], this.state.dates[1]];
+	        query.values = [this.state.dates[1], this.state.dates[0]];
 
 	        request.get('/mysql', {query:query}).then(response => {
 	            var tickers = response.body;
@@ -123,11 +151,18 @@ class NagotSomFunkarBattreOmNagotBlirFel extends React.Component {
 		this.getFearAndGreed().then(body => {
 			this.getDates().then(dates => {
 		        this.setState({dates:dates});
-				this.getSectors().then(sectors => {
-			        this.setState({sectors:sectors});
-			        this.getSpikes().then(spikes => {
-			            this.setState({spikes:spikes});
-			        })
+				this.getMMTH().then(perc => {		   
+			        this.setState({mmth:perc+"%"});					     
+					this.getSectors().then(sectors => {
+				        this.setState({sectors:sectors});
+				        this.getSpikes().then(spikes => {
+				            this.setState({spikes:spikes});
+				        })
+				        .catch(error => {
+				            console.log(error);
+				            this.setState({error:error});
+				        });
+				    })
 			        .catch(error => {
 			            console.log(error);
 			            this.setState({error:error});
@@ -136,7 +171,7 @@ class NagotSomFunkarBattreOmNagotBlirFel extends React.Component {
 		        .catch(error => {
 		            console.log(error);
 		            this.setState({error:error});
-		        });
+		        });			        
 		    })
 	        .catch(error => {
 	            console.log(error);
@@ -149,6 +184,20 @@ class NagotSomFunkarBattreOmNagotBlirFel extends React.Component {
         })
     }
     
+    getTopSectors() {
+		this.state.sectors.sort(function(a, b){return a.perc>b.perc});	    
+		
+		return (
+			<span><small>
+			1. {this.state.sectors[0].industry}<br/>
+			2. {this.state.sectors[1].industry}<br/>
+			3. {this.state.sectors[2].industry}<br/>
+			4. {this.state.sectors[3].industry}<br/>
+			5. {this.state.sectors[4].industry}<br/>			
+			</small></span>			
+		);
+    }
+    
 	handleCheck = (childData) => {		
 		this.setState({tickers: this.state.tickers == "" ? childData : this.state.tickers + "\n" + childData});		
 	}
@@ -156,7 +205,7 @@ class NagotSomFunkarBattreOmNagotBlirFel extends React.Component {
 	handleChange(event) {
 		this.setState({tickers: event.target.value});
 	}
-	
+
     render() {
         if (this.state.spikes) {
 	        
@@ -164,12 +213,31 @@ class NagotSomFunkarBattreOmNagotBlirFel extends React.Component {
             style.display = 'block';
             style.marginLeft = 'auto';
             style.marginRight = 'auto';
-            style.resize = 'none';	        
+            style.resize = 'none';	  
+            
+			var styleContainer = {};
+			styleContainer.borderColor = 'rgba(0, 0, 0, 0.1)';
+			styleContainer.borderWidth = '1px';
+			styleContainer.borderStyle = 'solid';
+			styleContainer.lineHeight = '90%';
 
 	        return (
 		        <div>
 		        <h1 className="text-center">{this.state.dates[1] + " - " + this.state.dates[0] + " (" + this.state.spikes.length + " st)"}</h1>
 		        <h4 className="text-center">({_descr1})</h4>
+		        <Container fluid={true}>
+                    <Container.Row style={styleContainer}>
+                    <Container.Col>
+		        	{this.getTopSectors()}
+                    </Container.Col>
+                    <Container.Col>
+		        	<h1 className="text-center">{this.state.mmth}</h1>
+                    </Container.Col>                    
+                    <Container.Col>
+		        	i lingonskogen
+                    </Container.Col>                    
+                    </Container.Row>		        	
+		        </Container>
                 <StockChartList symbols={this.state.spikes} sectors={this.state.sectors} tickers={this.state.tickers} callback={this.handleCheck}/>
                 <textarea style={style} name="candidates" rows="4" cols="100" placeholder="Kandidater" value={this.state.tickers} onChange={this.handleChange}></textarea>
                 </div>
